097fdba085c43674b68c64eb39cc7a51600bfd92
killper <2721366730@qq.com>
2024-10-31 09:25:21 +0800

    完成作业

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include ".assignment/test.h"
 
-// 比较三个数的大小
-void CompareThreeNumbers(int a, int b, int c, Result &result)
+// 姣旇緝涓変釜鏁扮殑澶у皬
+void CompareThreeNumbers(int a, int b, int c, Result& result)
 {
-    // 在这里完成你的代码
+    // 浣犵殑绠楁硶
+
+    result.min = (a < b) ? (a < c ? a : c) : (b < c ? b : c);
+
+    if ((a > result.min && a < result.max)) {
+        result.mid = a;
+    } 
+    else if ((b > result.min && b < result.max)) {
+        result.mid = b;
+    } 
+    else {
+        result.mid = c;
+    }
+
+    result.max =(a > b) ? (a > c ? a : c) : (b > c ? b : c);
+    return result;
 }
